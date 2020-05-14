d25a24a6d52f5f146750fe2ec25d05bd7ec97581
Daniel Pavela <daniel.pavela@gmail.com>
2020-05-14 10:12:35 +1000

    Updated compile script

--- compile.sh
+++ compile.sh
@@ -3,12 +3,12 @@
 mkdir bin.build
 python3 -m nuitka --standalone --remove-output --output-dir=bin.build main.py
 if [[ "$OSTYPE" == "darwin"* ]]; then
-	mv ./DungeonCli.bin ./bin.build/DungeonCli_osx
-	sudo codesign -f -s - ./bin.build/DungeonCli.dist/Python
+	mv ./autoClicker.bin ./bin.build/autoClicker_osx
+	sudo codesign -f -s - ./bin.build/autoClicker.dist/Python
 elif [[ "$OSTYPE" == "linux-gnu" ]]; then
-	mv ./DungeonCli.bin ./bin.build/DungeonCli_linux
+	mv ./autoClicker.bin ./bin.build/autoClicker_linux
 else
-	mv ./DungeonCli.bin ./bin.build/DungeonCli
+	mv ./autoClicker.bin ./bin.build/autoClicker
 fi
 
 cp -r ./Music ./bin.build/DungeonCli.dist/
