e09308e0c66c4b02fbb120a54cb848c255ef1034
Daniel Pavela <daniel.pavela@gmail.com>
2020-05-14 10:02:33 +1000

    Added compile script

---
+++ compile.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+mkdir bin.build
+python3 -m nuitka --standalone --remove-output --output-dir=bin.build main.py
+if [[ "$OSTYPE" == "darwin"* ]]; then
+	mv ./DungeonCli.bin ./bin.build/DungeonCli_osx
+	sudo codesign -f -s - ./bin.build/DungeonCli.dist/Python
+elif [[ "$OSTYPE" == "linux-gnu" ]]; then
+	mv ./DungeonCli.bin ./bin.build/DungeonCli_linux
+else
+	mv ./DungeonCli.bin ./bin.build/DungeonCli
+fi
+
+cp -r ./Music ./bin.build/DungeonCli.dist/
+cp -r ./Sounds ./bin.build/DungeonCli.dist/
